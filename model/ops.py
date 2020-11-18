c69ff17dd2dbd8c70d217413360de7b62d9f2773
Richard Kuo <rkuo2000@gmail.com>
2020-11-19 07:35:49 +0800

    Update ops.py

--- model/ops.py
+++ model/ops.py
@@ -5,34 +5,34 @@ import tensorflow as tf
 
 
 def batch_norm(x, is_training, epsilon=1e-5, decay=0.9, scope="batch_norm"):
-    return tf.contrib.layers.batch_norm(x, decay=decay, updates_collections=None, epsilon=epsilon,
+    return tf.compat.v1.layers.batch_normalization(x, decay=decay, updates_collections=None, epsilon=epsilon,
                                         scale=True, is_training=is_training, scope=scope)
 
 
 def conv2d(x, output_filters, kh=5, kw=5, sh=2, sw=2, stddev=0.02, scope="conv2d"):
-    with tf.variable_scope(scope):
+    with tf.compat.v1.variable_scope(scope):
         shape = x.get_shape().as_list()
-        W = tf.get_variable('W', [kh, kw, shape[-1], output_filters],
+        W = tf.compat.v1.get_variable('W', [kh, kw, shape[-1], output_filters],
                             initializer=tf.truncated_normal_initializer(stddev=stddev))
         Wconv = tf.nn.conv2d(x, W, strides=[1, sh, sw, 1], padding='SAME')
 
-        biases = tf.get_variable('b', [output_filters], initializer=tf.constant_initializer(0.0))
+        biases = tf.compat.v1.get_variable('b', [output_filters], initializer=tf.constant_initializer(0.0))
         Wconv_plus_b = tf.reshape(tf.nn.bias_add(Wconv, biases), Wconv.get_shape())
 
         return Wconv_plus_b
 
 
 def deconv2d(x, output_shape, kh=5, kw=5, sh=2, sw=2, stddev=0.02, scope="deconv2d"):
-    with tf.variable_scope(scope):
+    with tf.compat.v1.variable_scope(scope):
         # filter : [height, width, output_channels, in_channels]
         input_shape = x.get_shape().as_list()
-        W = tf.get_variable('W', [kh, kw, output_shape[-1], input_shape[-1]],
+        W = tf.compat.v1.get_variable('W', [kh, kw, output_shape[-1], input_shape[-1]],
                             initializer=tf.random_normal_initializer(stddev=stddev))
 
         deconv = tf.nn.conv2d_transpose(x, W, output_shape=output_shape,
                                         strides=[1, sh, sw, 1])
 
-        biases = tf.get_variable('b', [output_shape[-1]], initializer=tf.constant_initializer(0.0))
+        biases = tf.compat.v1.get_variable('b', [output_shape[-1]], initializer=tf.constant_initializer(0.0))
         deconv_plus_b = tf.reshape(tf.nn.bias_add(deconv, biases), deconv.get_shape())
 
         return deconv_plus_b
@@ -43,27 +43,27 @@ def lrelu(x, leak=0.2):
 
 
 def fc(x, output_size, stddev=0.02, scope="fc"):
-    with tf.variable_scope(scope):
+    with tf.compat.v1.variable_scope(scope):
         shape = x.get_shape().as_list()
-        W = tf.get_variable("W", [shape[1], output_size], tf.float32,
+        W = tf.compat.v1.get_variable("W", [shape[1], output_size], tf.float32,
                             tf.random_normal_initializer(stddev=stddev))
-        b = tf.get_variable("b", [output_size],
+        b = tf.compat.v1.get_variable("b", [output_size],
                             initializer=tf.constant_initializer(0.0))
         return tf.matmul(x, W) + b
 
 
 def init_embedding(size, dimension, stddev=0.01, scope="embedding"):
-    with tf.variable_scope(scope):
-        return tf.get_variable("E", [size, 1, 1, dimension], tf.float32,
-                               tf.random_normal_initializer(stddev=stddev))
+    with tf.compat.v1.variable_scope(scope):
+        return tf.compat.v1.get_variable("E", [size, 1, 1, dimension], tf.float32,
+                               tf.compat.v1.random_normal_initializer(stddev=stddev))
 
 
 def conditional_instance_norm(x, ids, labels_num, mixed=False, scope="conditional_instance_norm"):
-    with tf.variable_scope(scope):
+    with tf.compat.v1.variable_scope(scope):
         shape = x.get_shape().as_list()
         batch_size, output_filters = shape[0], shape[-1]
-        scale = tf.get_variable("scale", [labels_num, output_filters], tf.float32, tf.constant_initializer(1.0))
-        shift = tf.get_variable("shift", [labels_num, output_filters], tf.float32, tf.constant_initializer(0.0))
+        scale = tf.compat.v1.get_variable("scale", [labels_num, output_filters], tf.float32, tf.constant_initializer(1.0))
+        shift = tf.compat.v1.get_variable("shift", [labels_num, output_filters], tf.float32, tf.constant_initializer(0.0))
 
         mu, sigma = tf.nn.moments(x, [1, 2], keep_dims=True)
         norm = (x - mu) / tf.sqrt(sigma + 1e-5)
